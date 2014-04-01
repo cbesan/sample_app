f82297a7df779609eb960b42b0ef3ddbd828a029
Juan Ruiz <invitado@perdidita.(none)>
2014-04-01 18:02:24 +0100

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
   has_secure_password
+  VALID_PASSWORD_REGEX = /\A[A-Z]+[[a-zA-Z]+\d+]+[A-Z]+\z/
   validates :password, length: { minimum: 6 }
 end
